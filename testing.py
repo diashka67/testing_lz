a3f644206c422f81336d4f8e810f63d0db5d44b6
diawsz67 <sneine.pepe666@atomicmail.io>
2026-06-17 13:41:56 +0300

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,30 @@
+import unittest
+from heand import hand
+
+class Test_hand(unittest.TestCase):
+
+    def test_by_zero(self):
+        self.assertEqual((2, 2, 4), 'Деление на ноль')
+        self.assertEqual((-2, -2, 4), 'Деление на ноль')
+
+    def test_all_zero(self):
+        self.assertEqual((0, 0, 0), 'Деление на ноль')
+
+    def test_normal(self):
+        self.assertEqual((2, -2, 4), 2.5)
+
+    def test_negativet(self):
+        self.assertEqual((1, 3, 9), 'Извлечение корня из отрицательного числа')
+        self.assertEqual((1, 0, -5), 'Извлечение корня из отрицательного числа')
+
+    def test_text(self):
+        self.assertEqual(("a", 1, 4), 'Ошибка типов данных')
+        self.assertEqual((1, "b", 4), 'Ошибка типов данных')
+        self.assertEqual((2, 1, "text"), 'Ошибка типов данных')
+
+    def test_empty(self):
+        self.assertEqual(("", 0, 4), 'Ошибка типов данных')
+        self.assertEqual((1, "", 4), 'Ошибка типов данных')
+
+if __name__ == "__main__":
+    unittest.main()
